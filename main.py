b686ea2dc393884a8cfe33213a79d6fc96efbf0a
Ganesh Dabhade <158286559+Ganeshd0003@users.noreply.github.com>
2026-01-23 01:52:48 +0530

    after work'

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ print("hello India")
 hideturtle()
 import os
 print(os.getcwd())
+
+o = 3
+match o:
+    case "1":
+        print("1")
+    case "2":
+        print("2")
+    case "3":
+        print("3")
+    case _:
+        print("something wrong")
